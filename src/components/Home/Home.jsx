5a37857c500fde293aeaf4d751f43d5810bd38ba
salioudia <diasaliou2@gmail.com>
2022-04-24 21:57:00 -0400

    Add ui for indded clone

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Menu from './Menu'
+import HomeForm from './HomeForm'
+
+function Home() {
+  return (
+    <div>
+
+        <Menu/>
+        <HomeForm/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Home/UI/Buttons.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Buttons() {
+  return (
+    <div>
+
+        <button className='search-button'>Rechercher</button>
+    </div>
+  )
+}
